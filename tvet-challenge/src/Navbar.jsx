cc634b093fde19c139d35b4726c6301cf09d4f3f
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-31 12:21:02 +0200

    refactor navbar for sytle

--- tvet-challenge/src/Navbar.jsx
+++ tvet-challenge/src/Navbar.jsx
@@ -4,16 +4,18 @@
 const Navbar =()=>{
     return(
         <>
-        <div className=" flex justify-between p-6">
+        <div className=" flex justify-between p-6 text-blue-700  items-center">
             <div>
                <h1>Log</h1> 
             </div>
-            <div className=' flex justify-between gap-5'>
+            <div className=' flex justify-between gap-5 items-center'>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/sectors">Sectors</Link>
             <Link to="/successStories">Succes Stories</Link>
             <Link to="/carriesPathWays">Carries Path Ways</Link>
+            <Link to="" className=" bg-green-500 rounded-2xl text-white font-bold p-3">Get Started</Link>
+            
             </div>
         </div>
         </>
