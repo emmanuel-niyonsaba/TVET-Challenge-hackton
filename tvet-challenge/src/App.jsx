5ed5c4bcbe34842224b9bea93ea2e8d6bd8e2491
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-31 11:19:11 +0200

    add tailwindcss  packege in project

--- tvet-challenge/src/App.jsx
+++ tvet-challenge/src/App.jsx
@@ -2,11 +2,13 @@
 import './App.css'
 
 function App() {
+  
 
-    
   return (
     <>
-    <h1>Hello world</h1>
+       <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
