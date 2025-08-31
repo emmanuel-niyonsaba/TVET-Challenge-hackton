106d2da949a80eb39775f6df358eb903d6da0b3f
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-31 12:13:38 +0200

    add all router for navbar

--- tvet-challenge/src/App.jsx
+++ tvet-challenge/src/App.jsx
@@ -6,16 +6,25 @@ import About from './pages/About'
 import Sectors from './pages/Sectors'
 import SuccessStories from './pages/SuccessStories'
 import CarriesPathWays from './pages/CarriesPathWays'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
+     
     <Navbar/>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+ 
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/sectors' element={<Sectors/>}></Route>
+      <Route path='/successStories' element={<SuccessStories/>}></Route>
+      <Route path='/carriesPathWays' element={<CarriesPathWays/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- tvet-challenge/src/Navbar.jsx
+++ tvet-challenge/src/Navbar.jsx
@@ -1,8 +1,21 @@
 
+
+ import { Link } from "react-router-dom"
 const Navbar =()=>{
     return(
         <>
-        <h1>This is navbar</h1>
+        <div className=" flex justify-between p-6">
+            <div>
+               <h1>Log</h1> 
+            </div>
+            <div className=' flex justify-between gap-5'>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/sectors">Sectors</Link>
+            <Link to="/successStories">Succes Stories</Link>
+            <Link to="/carriesPathWays">Carries Path Ways</Link>
+            </div>
+        </div>
         </>
     )
 }
