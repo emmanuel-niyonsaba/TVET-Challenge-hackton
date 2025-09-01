be11a3a63e6de091161ba6d26965dfae7c214f82
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-09-01 12:09:33 +0200

    working on the numbers section

--- tvet-challenge/src/pages/Home.jsx
+++ tvet-challenge/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import secondImg from '../assets/landingPage/secondImg.jpg'
 import thirdImg from '../assets/landingPage/thirdImg.jpg'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import firstAd from '../assets/landingPage/firstAd.png';
+import secondAd from '../assets/landingPage/secondAd.png'
 const Home = () => {
     const slides = [
         <div className='w-full relative'>
@@ -76,44 +78,82 @@ const Home = () => {
                         <p className='text-2xl poppins-extralight w-full'>Announcements</p>
                         <ChevronRightIcon className='text-3xl text-black' />
                     </div>
-                    <div className='w-full flex flex-row space-x-3'>
-                        <div className='w-1/2 text-2xl'>
-                            <h1 className='font-bold  poppins-extralight'>
+                    <div className='w-full flex flex-row space-x-3 h-[80vh]'>
+                        <div className="w-1/2 text-xl bg-gradient-to-r from-blue-50 to-blue-100 shadow-lg rounded-2xl p-4 flex flex-col items-center text-center h-full animate-pulse">
+                            <h1 className="font-bold poppins-extralight mb-4 ">
                                 ✨ 🚀 Ready to Kick-Start Your ICT Career? ✨
                             </h1>
 
-                            <p className='poppins text-xl'>
-                                💼 [XXX Company] is excited to announce that we’re happily open to hiring passionate Web Developers!
+                            <p className="poppins text-xl mb-3">
+                                💼 [XXX Company] is thrilled to welcome passionate Web Developers to join our growing team!
                             </p>
 
-                            <h2 className='font-semibold mt-5'>
-                                👩‍💻 What we’re looking for:  
+                            <h2 className="font-semibold mb-3">
+                                👩‍💻 What We’re Looking For:
                             </h2>
-                            <ul className='flex flex-col items-center'>
-                                <li>
-                                    🌐 Web Development skills
-                                </li>
-                                <li>
-                                    🎨 UI/UX Design expertise
 
-                                </li>
-                                <li>
+                            <ul className="flex flex-col gap-2 mb-3">
+                                <li>🌐 Web Development skills</li>
+                                <li>🎨 UI/UX Design knowledge</li>
+                                <li>🗣️ Great Communication skills</li>
+                            </ul>
 
-                                    🗣️ Strong Communication skills
-                                </li>
-                            </ul><br />
+                            <p className="mb-4">
+                                ✍️ Even if you’ve only taken ICT short courses — you’re still encouraged to apply!
+                            </p>
 
+                            <p className="font-semibold mb-6">
+                                "We believe passion and determination matter more than the length of your studies."
+                            </p>
 
-
-                            Even if you only took short ICT courses — you’re still welcome to apply!
-                            <p className='font-semibold'>"We believe talent and determination matter more than the length of your study."</p>
-                            👉 If you’re motivated, creative, and eager to grow, this opportunity is for YOU!
+                            <span className="text-lg font-medium">
+                                👉 If you’re curious, creative, and eager to grow, this opportunity is made for YOU!
+                            </span>
                         </div>
-                        <div className='w-1/2'>
-                            <img src={firstImg} alt="" className='object-cover' />
+
+
+                        <div className='w-1/2 flex flex-row space-x-3'>
+                            <div className='relative w-1/2 hover:cursor-pointer hover:animate-pulse'>
+                                <img src={firstAd} alt="" className='w-full h-full object-cover rounded-lg' />
+                                <div className='absolute inset-0  px-2'>
+                                    <button className='bg-[#00c951] text-white p-2  rounded-sm mt-3 ml-45'>Enroll Now</button>
+                                </div>
+                            </div>
+                            <div className='relative w-1/2 hover:cursor-pointer hover:animate-pulse'>
+                                <img src={secondAd} alt="" className='w-full h-full object-cover rounded-lg' />
+                                <div className='absolute inset-0'>
+                                    <button className='bg-[#00c951] text-white p-2  rounded-sm mt-3 ml-45'>Enroll Now</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className='flex flex-col space-y-5 px-4'>
+                    <div className='flex flex-row justify-between'>
+                        <p className='text-2xl poppins-extralight w-full'>Numbers</p>
+                        <ChevronRightIcon className='text-3xl text-black' />
+                    </div>
+                    <div className='w-full flex flex-row justify-around items-center'>
+                        <div className='flex flex-col space-y-4 items-center'>
+                            <h1 className='text-[#00c951] text-3xl font-bold'>238</h1>
+                            <p className='poppins'>Graduates</p>
+                        </div>
+                        <div className='flex flex-col space-y-4 items-center'>
+                            <h1 className='text-[#00c951] text-3xl font-bold'>248</h1>
+                            <p className='poppins'>Internships</p>
+                        </div>
+                        <div className='flex flex-col space-y-4 items-center'>
+                            <h1 className='text-[#00c951] text-3xl font-bold'>68</h1>
+                            <p className='poppins'>Active jobs</p>
+                        </div>
+                        <div className='flex flex-col space-y-4 items-center'>
+                            <h1 className='text-[#00c951] text-3xl font-bold'>118</h1>
+                            <p className='poppins'>Private Sectors</p>
+                        </div>
+                    </div>
+                </div>
+                
+
             </div>
         </>
     )
