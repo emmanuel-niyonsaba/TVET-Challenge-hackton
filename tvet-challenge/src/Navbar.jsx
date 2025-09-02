81a1863a5b2864557b1b8ecc6ef1703110b94328
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-09-02 08:09:01 +0200

    refact: add the google translate

--- tvet-challenge/src/Navbar.jsx
+++ tvet-challenge/src/Navbar.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import dark from "./assets/dark-icon.png";
 import log from "./assets/log1.png";
+import Language from "./hooks/Language";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
-
+  const googleTranslateRef = useRef(null)
   
   useEffect(() => {
     if (darkMode) {
@@ -17,14 +18,40 @@ const Navbar = () => {
     }
   }, [darkMode]);
 
-  const handleLanguageChange = (lang) => {
-    const select = document.querySelector(".goog-te-combo");
-    if (select) {
-      select.value = lang;
-      select.dispatchEvent(new Event("change"));
-    }
+    const handleLanguageChange = (value) => {
+      const lang = value.code;
+      const combo = document.querySelector(".goog-te-combo");
+       console.log(combo)
+      if (combo) {
+        combo.value = lang;
+        console.log(combo)
+        console.log(combo.value)
+        const event = new Event("change");
+        combo.dispatchEvent(event);
+      }
   };
 
+ useEffect(()=>{
+      let intervalId;
+            const googleTranslateElementInit = () => {
+       if(window.google && window.google.translate){
+        clearInterval(intervalId);
+  
+        new window.google.translate.TranslateElement(
+        {
+          pageLanguage: "en",
+          autoDisplay: false,
+          includedLanguages: "fr,es,sw,rw,en",
+          layout:window.google.translate.TranslateElement.FloatPosition.TOP_LEFT
+        },
+        googleTranslateRef.current
+      );
+       }
+    }; 
+    intervalId = setInterval(googleTranslateElementInit,100)
+    
+    },[])
+    
   return (
     <div
       className="flex justify-between p-6 items-center"
@@ -38,7 +65,7 @@ const Navbar = () => {
       </div>
       <div className="flex justify-between gap-5 items-center">
         {/* Language Selector */}
-        <select
+        {/* <select
           onChange={(e) => handleLanguageChange(e.target.value)}
           className="border-2 px-2 py-1"
           style={{
@@ -52,8 +79,10 @@ const Navbar = () => {
           <option value="sw">Swahili</option>
           <option value="ar">العربية</option>
           <option value="rw">Kinyarwanda</option>
-        </select>
+        </select> */}
 
+                            <div id="google_translate_element" style={{display: "none"}} ref={googleTranslateRef}></div>
+                          <Language onLanguageChange={handleLanguageChange} className='bg-transparent w-full' />
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/sectors">Sectors</Link>
